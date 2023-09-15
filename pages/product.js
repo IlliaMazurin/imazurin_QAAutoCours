877ede8cd6ea68ac2641265f38896795c6b809e1
Illia.Mazurin <illia.mazurin@magicpowered.io>
2023-09-15 22:44:29 +0300

    data dreven

--- pages/product.js
+++ pages/product.js
@@ -3,42 +3,60 @@ const { I } = inject();
 module.exports = {
 
   priceNew : { xpath : '//span[@class="price-new"]' },
+  fullprice : { xpath : '//div[@class="price"]/span' },
   productTitle : { xpath : '//h2[@class="product-title"]' },
-  sizeDropdown : {xpath : '//label[@for="input-option22"]/following-sibling::div' },
-  colorDropdown : {xpath : '//label[@for="input-option21"]/following-sibling::div' },
-  sizeOption : { xpath : '//a[contains(text(), "Medium")]' },
-  colorOption : { xpath : '//a[contains(text(), "White")]' },
+  sizeDropdown : {xpath : '//label[text()="Size"]/following-sibling::div' },
+  colorDropdown : {xpath : '//label[text()="Color"]/following-sibling::div' },
+  // sizeSmallOption : { xpath : '//a[contains(text(), "Small")]' },
+  // sizeMediumOption : { xpath : '//a[contains(text(), "Medium")]' },
+  // sizeLargeOption : { xpath : '//a[contains(text(), "Large")]' },
+  // colorWhiteOption : { xpath : '//a[contains(text(), "White")]' },
+  // colorGreenOption : { xpath : '//a[contains(text(), "Green")]' },
+  // colorGrayOption : { xpath : '//a[contains(text(), "Gray")]' },
+  // colorBrownOption : { xpath : '//a[contains(text(), "Brown")]' },
+  // colorBlackOption : { xpath : '//a[contains(text(), "Black")]' },
   addButtonCart : { xpath : '//button[@id="button-cart"]' },
 
-  selectColor () {
-    I.click(this.colorDropdown);
-    I.click(this.colorOption);
+  async selectColor (color) {
+    if (await this.checkElementExist(this.colorDropdown)) {
+      I.click(this.colorDropdown);
+      I.click(`//a[contains(text(), "${color}")]`);
+      const colorPrice = await I.grabTextFrom(`//a[contains(text(), "${color}")]`);
+      price = I.getPriceFromString(colorPrice);
+    } else {
+      price = 0;
+    }
+    return +price;
   },
 
-  selectSize () {
-    I.click(this.sizeDropdown);
-    I.click(this.sizeOption);
+  async selectSize (size) {
+    if (await this.checkElementExist(this.sizeDropdown)) {
+      I.click(this.sizeDropdown);
+      I.click(`//a[contains(text(), "${size}")]`);
+      const sizePrice = await I.grabTextFrom(`//a[contains(text(), "${size}")]`);
+      price = I.getPriceFromString(sizePrice);
+    } else {
+      price = 0;
+    }
+    return +price;
   },
 
   addToCart () {
     I.click(this.addButtonCart);
   },
 
-  async getProductNewPrice () {
-    const newPrice = await I.grabTextFrom(this.priceNew);
-    price = I.getPriceFromString(newPrice);
-    return +price;
-  },
-  async getColorProductPrice () {
-    const colorPrice = await I.grabTextFrom(this.colorOption);
-    price = I.getPriceFromString(colorPrice);
-    return +price;
-  },
-  async getSizeProductPrice () {
-    const sizePrice = await I.grabTextFrom(this.sizeOption);
-    price = I.getPriceFromString(sizePrice);
+  async getProductPrice () {
+    if (await this.checkElementExist(this.priceNew)) {
+      const priceString = await I.grabTextFrom(this.priceNew);
+      price = I.getPriceFromString(priceString);
+    } else {
+      const priceString = await I.grabTextFrom(this.fullprice);
+      price = I.getPriceFromString(priceString);
+    }
     return +price;
   },
 
-
+  async checkElementExist(element) {
+    return await tryTo(() => I.seeElement(element));
+  }
 }
